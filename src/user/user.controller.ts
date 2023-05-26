d6e6a8b3a2cd6077a318f6c3e8fe9e9651d78114
Thomas Rousseau <thomas.rousseau@proton.me>
2023-05-26 11:23:10 +0200

    API - User login classique

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,13 +2,19 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserSubscribeDto } from './dto/user-subscribe.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
+import { LoginCredentialsDto } from './dto/login-credentials-dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Post()
+  @Post('subscribe')
   register(@Body() userData: UserSubscribeDto): Promise<Partial<User>> {
     return this.userService.register(userData);
+  }
+
+  @Post('login')
+  login(@Body() credentials: LoginCredentialsDto): Promise<Partial<User>> {
+    return this.userService.login(credentials);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,14 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserSubscribeDto } from './dto/user-subscribe.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
+import { LoginCredentialsDto } from './dto/login-credentials-dto';
 
 @Injectable()
 export class UserService {
@@ -31,6 +36,36 @@ export class UserService {
       id: user.id,
       username: user.username,
       email: user.email,
+      role: user.role,
+    };
+  }
+
+  async login(credentials: LoginCredentialsDto): Promise<Partial<User>> {
+    const { email, password } = credentials;
+
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', {
+        email,
+      })
+      .getOne();
+
+    if (!user) {
+      throw new NotFoundException(
+        "Le nom d'utilisateur ou le mot de passe est incorrect.",
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(password, user.salt);
+    if (hashPassword !== user.password) {
+      throw new NotFoundException(
+        "Le nom d'utilisateur ou le mot de passe est incorrect.",
+      );
+    }
+
+    return {
+      username: user.username,
+      email: email,
       role: user.role,
     };
   }
